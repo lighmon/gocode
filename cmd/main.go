ecfa7480855bd971961b340a131434fe620c4835
Lighmon <Ligh_mon@outlook.com>
2024-07-17 12:25:55 +0800

    test gomod

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,9 @@
 package main
+
+import (
+	"github.com/lighmon/gocode/pkg/ptf"
+)
+
+func main() {
+	ptf.Print("鹿野灸")
+}
---
+++ pkg/ptf/ptf.go
@@ -0,0 +1,7 @@
+package ptf
+
+import "fmt"
+
+func Print(a ...interface{}) {
+	fmt.Println(a...)
+}
